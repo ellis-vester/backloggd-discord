6c7fd3ffa2311f8a2360efad0fe97d6026b9d80a
ellis-vester <ellis_vester@protonmail.com>
2025-03-06 21:18:24 -0500

    Unwrapped config binding POCO

--- dotnet/Options/BotConfig.cs
+++ dotnet/Options/BotConfig.cs
@@ -1,10 +1,5 @@
 namespace BackloggdBot.Options;
 
-public sealed class Settings
-{
-    public required BotConfig BotConfig { get; set; }
-}
-
 public sealed class BotConfig
 {
     public required string Token { get; set; }
